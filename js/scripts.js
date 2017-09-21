0dc82e98786b507e91dba5c21b2037f3a174f681
Krzych <krzych.pierzchala@gmail.com>
2017-09-21 07:24:50 +0200

    Fixed js file once again

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var a = prompt("Wpisz pierwszą liczbę:");
-var	b = prompt("Wpisz drugą liczbę:");
-var	value = (a * a) + (2 * a * b) - (b * b);
+var b = prompt("Wpisz drugą liczbę:");
+var value = (a * a) + (2 * a * b) - (b * b);
 
 console.log('Twój wynik to: ' + value);
 if (value > 0) console.log('wynik dodatni');
